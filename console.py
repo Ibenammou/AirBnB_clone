988b4d0f044e16c23faf6c6342618cede73713c0
Ibenammou <imanebenamou@gmail.com>
2024-01-11 10:28:13 -0800

    it's not complete

--- console.py
+++ console.py
@@ -1,25 +1,3 @@
-"""#!/usr/bin/python3
-
-import cmd
-
-
-class HBNBCommand(cmd.Cmd):
-
-    prompt = '(hbnb) '
-
-    def do_quit(self, arg):
-        return True
-
-    def do_EOF(self, arg):
-    
-        return True
-
-    def emptyline(self):
-        pass
-
-
-if __name__ == '__main__':
-    HBNBCommand().cmdloop()"""
 #!/usr/bin/python3
 """Console Interpreter"""
 import cmd
