4956b506941b97ebfa051db9b995676319f8e5b1
Ruhinda Roderick Izooba <rickyruhinda@gmail.com>
2024-01-14 09:06:22 -0800

    Fix

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,6 @@
 
 import uuid
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
@@ -35,7 +34,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new()
+            from models import storage
+            storage.new(self)
 
     def __str__(self):
         """
@@ -48,6 +48,8 @@ class BaseModel:
         """
         Save method updates the public instance attribute - updated_at
         """
+        from models import storage
+
         self.updated_at = datetime.now()
         storage.save()
 
