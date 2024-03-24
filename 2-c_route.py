f9987ebd8c86cd4b2bb2b722aea4631f9c13d1a3
root <imanebenamou@gmail.com>
2024-03-24 20:54:06 +0000

    task

--- 2-c_route.py
+++ 2-c_route.py
@@ -1,37 +1,33 @@
 #!/usr/bin/python3
-"""
-Script to handle dynamic routes
+""" script that starts a Flask web application
+-Your web application must be listening on 0.0.0.0, port 5000
+-Routes:
+        /: display “Hello HBNB!”
+        /hbnb: display “HBNB”
+        /c/<text>: display “C ” followed by the value of the
+        text variable (replace underscore _ symbols with a space )
+-You must use the option strict_slashes=False in your route definition
 """
 
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """ Returns - Hello HBNB """
+def hello_hbnb():
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """ Returns HBNB """
+def hello():
     return 'HBNB'
 
 
-@app.route('/custom404', strict_slashes=False)
-def custom404():
-    """ Route to trigger a 404 error """
-    return render_template('404.html'), 404
-
-
 @app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """ Replace underscores with spaces"""
-    formatted_text = text.replace('_', ' ')
-    return 'C {}'.format(formatted_text)
+def c_is_fun(text):
+    return 'C {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
